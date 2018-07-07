26b1afd151d12ca796ede8f8133370130bed0ea1
Claude Meurehg <claude.meurehg@mail.utoronto.ca>
2018-07-06 20:11:50 -0400

    connection working and products listed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,24 +1,36 @@
 var mysql = require ("mysql2");
 var inquirer = require ("inquirer");
 
-var connect = mysql.createConnection ({
+var connection = mysql.createConnection ({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "M0v1ng0n#",
     database: "bamazon"
 });
 
-var list = function (products) {
-    for(p = 0; p < products.length; p++) {
-        console.log ("Product ID:", products[p].item_id);
-        console.log ("Product Name:", products[p].product_name);
-        console.log ("Price:", products[p].price);
-    }
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    listProducts();
+  });
+
+// var list = function (products) {
+//     for(p = 0; p < products.length; p++) {
+//         console.log ("Product ID:", products[p].item_id);
+//         console.log ("Product Name:", products[p].product_name);
+//         console.log ("Price:", products[p].price);
+//     }
     
+// };
+
+function listProducts() {
+    console.log("Listing all products:\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      console.log(res);
+    });
 };
-
-
   
   var questions = [
  
@@ -33,3 +45,5 @@ var list = function (products) {
   }];
 
   inquirer.prompt(questions);
+
+  
